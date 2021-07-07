49c1e49969ef8935b786b933099322a11b3a20ff
Nicole B <nicole@faenre.dev>
2021-07-07 15:23:27 -0400

    added dummy data

--- main.go
+++ main.go
@@ -47,7 +47,7 @@ func load_cfg() {
 
 
 // zipkin data format
-type SpanStruct struct {
+type SpanStructInput struct {
   trace_id      string `json:"traceId"`
   span_id       string `json:"id"`
   session_id    string `json:"tags.frontendSession"`
@@ -57,10 +57,25 @@ type SpanStruct struct {
   trigger_route string `json:"http.route"`
 
   time_sent     string `json:"timestamp"`
-  status_code   string `json:"tags.http.status_code`
+  status_code   int16  `json:"tags.http.status_code`
   span_data     string `json:"updatedSpanTags"`
 }
 
+
+
+type SpanStructFromDB struct {
+  trace_id      string `json:"traceId"`
+  span_id       string `json:"id"`
+  session_id    string `json:"tags.frontendSession"`
+  user_id       string `json:"tags.frontendUser"`
+
+  // trigger_route string `json:spanTags["http.method"] + " " + spanTags["http.route"]"`,
+  trigger_route string `json:"http.route"`
+
+  time_sent     string `json:"timestamp"`
+  status_code   int16  `json:"tags.http.status_code`
+  span_data     string `json:"updatedSpanTags"`
+}
 
 
 
@@ -70,109 +85,32 @@ func db_init() {}
 
 func post_to_db() {}
 
-/*
-  var collection *mongo.Collection
-  var collections = make(map[string]*mongo.Collection)
-  var ctx = context.TODO()
-
-  func db_init(uri string) {
-    clientOptions := options.Client().ApplyURI(uri)
-
-    client, err := mongo.Connect(ctx, clientOptions)
-    if err != nil {
-      log.Fatal(err)
-    }
-
-    err = client.Ping(ctx, nil)
-    if err != nil {
-      log.Fatal(err)
-    }
-
-    collection = client.Database("tracing").Collection("logs")
-    for _, source := range(cfg.Sources) {
-      collections[source] = client.Database("tracing").Collection(source)
-    }
-  }
-
-  func post_to_db(collection *mongo.Collection, td TraceData) {
-    if _, err := collection.InsertOne(context.TODO(), td); err != nil {
-      output("Error writing!")
-    } else {
-      output("Write success, into:", td.Collection)
-    }
-  }
-//*/
-
 
 
 /* web server */
-// func get_source_from_headers(headers map[string][]string) string {
-//   header := headers[cfg.SourceHeader]
-//   if headers == nil { return cfg.DefaultSource }
-//   if len(header) == 0 { return cfg.DefaultSource }
-
-//   source := header[0]
-//   _, ok := collections[source]
-//   if !ok { return cfg.DefaultSource }
-
-//   return source
-// }
-
-func form_handler(w http.ResponseWriter, r *http.Request) {
-  // if err := r.ParseForm(); err != nil { return }
-
-  // enableCors(&w)
-
-  // headers := headers_to_map(r.Header)
-
-  // collectionName := get_source_from_headers(headers)
-  // collection := collections[collectionName]
-
-  // buf, _ := ioutil.ReadAll(r.Body)
-  // content := fmt.Sprintf("%q", buf[:])
-
-  // td := TraceData{headers, collectionName, content}
-
-  // post_to_db(collection, td)
-}
-
-// func enableCors(w *http.ResponseWriter) {
-//   allowedHeaders := "*" // "Accept, Content-Type, Content-Length, Accept-Encoding, Authorization,X-CSRF-Token, Tracer-Source"
-//   (*w).Header().Set("Access-Control-Allow-Origin", "*")
-//   (*w).Header().Set("Access-Control-Allow-Methods", "POST, GET, OPTIONS, PUT, DELETE")
-//   (*w).Header().Set("Access-Control-Allow-Headers", allowedHeaders)
-//   (*w).Header().Set("Access-Control-Expose-Headers", "Authorization")
-// }
-
-// func headers_to_map(headers http.Header) map[string][]string {
-//   m := make(map[string][]string)
-//   for h, va := range(headers) {
-//     for _, v := range(va) { m[h] = append(m[h], v) }
-//   }
-//   return m
-// }
-
-func router(w http.ResponseWriter, r *http.Request) {
-}
 
 func root_path() string {
   return "hello"
 }
 
 func get_all_spans(w http.ResponseWriter, r *http.Request) {
-  fmt.Fprintf(w, "this is a test");
+  w.Header().Set("Content-Type", "application/json")
+  fmt.Fprintf(w,
+  `[{"trace_id": "1", "span_id": "2", "session_id": "3", "user_id": "4", "time_sent": "1299038700000", "trigger_route": "/some/place", "status_code": 200, "data": {} }]`)
 }
 
 func get_all_events(w http.ResponseWriter, r *http.Request) {
-
+  w.Header().Set("Content-Type", "application/json")
+  fmt.Fprintf(w,
+  `[{"id": "1", "session_id": "2", "user_id": "3", "time_sent": "1299038700000", "data": {} }]`)
 }
 
 func insert_span(w http.ResponseWriter, r *http.Request) {
-
+  w.WriteHeader(http.StatusOK)
 }
 
 func insert_event(w http.ResponseWriter, r *http.Request) {
-
+  w.WriteHeader(http.StatusOK)
 }
 
 /* orchestrate */
