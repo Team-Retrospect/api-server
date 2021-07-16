aae9433fe9774d3cbde71a9a10783c67e6cecbf9
Nicole B <nicole@faenre.dev>
2021-07-16 13:08:59 -0400

    secondary merge commit

--- src/webserver/helpers.go
+++ src/webserver/helpers.go
@@ -12,41 +12,67 @@ import (
 )
 
 func format_spans(blob []byte) []*structs.CassandraSpan {
-  // initializing an array of structs.SpanStructInput objects
-  var jspans []*structs.SpanStructInput
+  // initializing an array of SpanStructInput objects
+  var jspans []*SpanStructInput
   json.Unmarshal(blob, &jspans)
+  cspans := make([]*CassandraSpan, len(jspans))
 
-  // convert them into cassandra-compatible structs
-  cspans := make([]*structs.CassandraSpan, len(jspans))
-
-  for _, e := range(jspans) {
+  for _, e := range jspans {
     if e == nil { continue }
+    if e.Tags["http.method"] == "OPTIONS" { continue }
+
     sc, _ := strconv.ParseInt(e.Tags["http.status_code"], 10, 64)
-    request_data := e.Tags["requestData"]
+    rd := e.Tags["requestData"]
+
+    // if it's a db span, add frontend session info
+    _, ok := e.Tags["db.system"]
+    if ok {
+      // get trace id
+      tId := e.Trace_id
+
+      oneSpan := get_span_by_trace(tId)
+
+      e.Tags["frontendChapter"] = oneSpan.Chapter_id
+      e.Tags["frontendSession"] = oneSpan.Session_id
+      e.Tags["frontendUser"] = oneSpan.User_id
+      e.Tags["triggerRoute"] = oneSpan.Trigger_route
+    }
+
     delete(e.Tags, "requestData")
 
-    // encase in JSON syntax
-    tags := "{";
-    for k, v := range(e.Tags) { tags += fmt.Sprintf(`"%s": "%s", `, k, v) }
+    tags := "{"
+    for k, v := range e.Tags {
+      tags += fmt.Sprintf(`"%s": "%s", `, k, v)
+    }
     tags = tags[0:len(tags)-2] + "}"
-    // escape where necessary to safeguard against injections
-    tags = strings.Replace(fmt.Sprint(tags), "'", "\\'", -1) //
 
-    cspans = append(cspans, &structs.CassandraSpan{
-      Trace_id:       e.Trace_id,
-      Span_id:        e.Span_id,
-      Time_sent:      e.Time_sent,
-      Duration:       strconv.Itoa(e.Duration) + "us",
-      Session_id:     e.Tags["frontendSession"],
-      User_id:        e.Tags["frontendUser"],
-      Chapter_id:     e.Tags["frontendChapter"],
-      Trigger_route:  e.Tags["triggerRoute"],
-      Request_data:   request_data,
-      Status_code:    int16(sc),
-      Data:           tags,
+    cspans = append(cspans, &CassandraSpan{
+      Trace_id:      e.Trace_id,
+      Span_id:       e.Span_id,
+      Time_sent:     e.Time_sent,
+      Duration:      strconv.Itoa(e.Duration) + "us",
+      Session_id:    e.Tags["frontendSession"],
+      User_id:       e.Tags["frontendUser"],
+      Chapter_id:    e.Tags["frontendChapter"],
+      Trigger_route: e.Tags["triggerRoute"],
+      Request_data:  rd,
+      Status_code:   int16(sc),
+      Data: strings.Replace(fmt.Sprint(tags), "'", "\\'", -1),
     })
   }
   return cspans
+}
+
+// internal method used to fix frontend session values in db spans
+func get_span_by_trace(traceId string) *CassandraSpan {
+  query := fmt.Sprintf("SELECT JSON chapter_id, user_id, session_id, trigger_route FROM project.spans WHERE trace_id='%s' LIMIT 1;", traceId)
+
+  j := enumerate_query(query)
+
+  var cspan *CassandraSpan
+  json.Unmarshal([]byte(j[0]), &cspan)
+
+  return cspan
 }
 
 func insert_events(w http.ResponseWriter, r *http.Request) {
