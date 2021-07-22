1407712f58237adbfe6f438d49a2b5a14d3f07d8
chylirk <chylirk@gmail.com>
2021-07-22 14:22:34 -0500

    Added snapshots and events by session

--- src/webserver/paths.go
+++ src/webserver/paths.go
@@ -105,6 +105,24 @@ func get_all_events_by_chapter(w http.ResponseWriter, r *http.Request) {
   fmt.Fprintf(w, js)
 }
 
+// --> GET /events_by_session/{id}
+func get_all_events_by_session(w http.ResponseWriter, r *http.Request) {
+  vars := mux.Vars(r);
+  session_id, ok := vars["id"]
+
+  if !ok {
+    fmt.Println("session_id is missing in parameters")
+  }
+
+  query := fmt.Sprintf("SELECT JSON * FROM project.events WHERE session_id='%s';", session_id);
+
+  j := enumerate_query(query)
+  js := fmt.Sprintf("[%s]", strings.Join(j, ", "))
+
+  w.Header().Set("Content-Type", "application/json")
+  fmt.Fprintf(w, js)
+}
+
 // --> POST /spans
 func insert_spans(w http.ResponseWriter, r *http.Request) {
   fmt.Println("Inserting a Span")
@@ -145,6 +163,24 @@ func insert_events(w http.ResponseWriter, r *http.Request) {
 // --> GET /events/snapshots
 func get_snapshots(w http.ResponseWriter, r *http.Request) {
   query := "SELECT JSON * FROM project.snapshots;"
+
+  j := enumerate_query(query)
+  js := fmt.Sprintf("[%s]", strings.Join(j, ", "))
+
+  w.Header().Set("Content-Type", "application/json")
+  fmt.Fprintf(w, js)
+}
+
+// --> GET /events/snapshots_by_session/{id}
+func get_all_snapshots_by_session(w http.ResponseWriter, r *http.Request) {
+  vars := mux.Vars(r);
+  session_id, ok := vars["id"]
+
+  if !ok {
+    fmt.Println("session_id is missing in parameters")
+  }
+
+  query := fmt.Sprintf("SELECT JSON * FROM project.snapshots WHERE session_id='%s';", session_id);
 
   j := enumerate_query(query)
   js := fmt.Sprintf("[%s]", strings.Join(j, ", "))
